74c34f59311f90d675a6e4636e3fc46548d4a3f1
LilMzki <Mi2uk1ikz12d@gmail.com>
2024-09-08 10:46:46 +0900

    Add scripts
    Add scripts used to solve competitive programming problems.

---
+++ Program - 3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//N行K列の行列に対して、その転置行列を求める
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int N = int.Parse(inputs[0]);
+        int K = int.Parse(inputs[1]);
+
+        int[,] numbers = new int[N, K];
+
+        for (int i = 0; i < N; i++)
+        {
+            inputs = Console.ReadLine().Split(' ');
+            for (int j = 0; j < K; j++)
+            {
+                numbers[i, j] = int.Parse(inputs[j]);
+            }
+        }
+
+        for (int i = 0; i < K; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (j == N - 1) Console.Write($"{numbers[j, i]}\n");
+                else Console.Write($"{numbers[j, i]} ");
+            }
+        }
+    }
+}
---
+++ Program - 4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    //三次元の立体のデータが入力され、それの正面から見た図を出力する問題 
+
+    static void Main()
+    {
+        int[] inputs = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+        int X = inputs[0];
+        int Y = inputs[1];
+        int Z = inputs[2];
+
+        char[,,] printDatas = new char[Z, X, Y];
+
+        for (int i = 0; i < Z; i++)
+        {
+            for (int j = 0; j < X; j++)
+            {
+                string input = Console.ReadLine();
+                var datas = input.ToCharArray();
+                for (int k = 0; k < Y; k++)
+                {
+                    printDatas[i, j, k] = datas[k];
+                }
+
+                if (j == X - 1)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        char[,] frontData = new char[Z, Y];
+
+        for (int i = 0; i < Z; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                for (int k = 0; k < X; k++)
+                {
+                    if (printDatas[i, k, j] == '#')
+                    {
+                        frontData[i, j] = '#';
+                        break;
+                    }
+                    else
+                    {
+                        frontData[i, j] = '.';
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < Z; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                Console.Write(frontData[i, j]);
+            }
+        }
+    }
+}
